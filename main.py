0cd25d45f292d197b4850e425db1a853c214da56
josecarlos1211 <naelyb.883@gmail.com>
2026-05-30 12:29:55 -0300

    Update Flask app to run on specified host and port
    Refactor Flask app to use environment variables for port and host.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import os
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
+# ... suas rotas continuam aqui ...
 @app.route('/')
 def home():
     return render_template('index.html')
 @app.route('/viu')
 def viu():
     return render_template('viu.html')
-app.run(debug=True)
+
+if __name__ == "__main__":
+    # O Render passa a porta correta por essa variável de ambiente
+    port = int(os.environ.get("PORT", 5000))
+    # É fundamental usar o host "0.0.0.0" em vez de "127.0.0.1"
+    app.run(host="0.0.0.0", port=port, debug=False)
